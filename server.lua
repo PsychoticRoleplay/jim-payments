13cc5cafb79b820b8dd3c0ac052c20240b01a3b8
Carmo <kingcarmottv@gmail.com>
2022-06-21 13:55:38 -0700

    Commands Revamp

--- server.lua
+++ server.lua
@@ -10,7 +10,7 @@ AddEventHandler('onResourceStart', function(resource) if GetCurrentResourceName(
 	for k, v in pairs(Config.Jobs) do if not QBCore.Shared.Jobs[k] then print("Jim-Payments: Config.Jobs searched for job '"..k.."' and couldn't find it in the Shared") end end
 end)
 
-QBCore.Commands.Add("cashregister", "Use mobile cash register", {}, false, function(source) TriggerClientEvent("jim-payments:client:Charge", source, {}, true) end)
+QBCore.Commands.Add("cashregister", "Use mobile cash register", {}, false, function(source) TriggerClientEvent("jim-payments:client:Charge", source, {}, true) end, 'admin')
 
 RegisterServerEvent('jim-payments:Tickets:Give', function(data, biller)
 	--Find the biller from their citizenid
